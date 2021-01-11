0c1b2fe61630340cfcda2dbb73df755eca7e87c6
Fi <50817438+ThatGuyNameZack@users.noreply.github.com>
2021-01-11 23:57:59 +0630

    Update performance_check.bat

--- batch files/performance_check.bat
+++ batch files/performance_check.bat
@@ -4,10 +4,10 @@ Echo opening Task Manager . . .
 taskmgr
 echo done opening Task Manager . . .
  
-echo opening MSI AFTERBURNER . . .
-D:>cd \MSI Afterburner
-START MSIAfterburner.exe
-echo done opening MSI AFTERBURNER . . .
+//echo opening MSI AFTERBURNER . . .
+//D:>cd \MSI Afterburner
+//START MSIAfterburner.exe
+//echo done opening MSI AFTERBURNER . . .
 
 echo done opening necessary programs . . .
  
